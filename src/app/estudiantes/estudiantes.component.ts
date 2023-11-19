8140350b5c56a53c21f0926aab16c936b8287721
Christopher <christopher.a31@hotmail.com>
2023-11-18 19:15:37 -0500

    Agregar botones editar y eliminar

--- src/app/estudiantes/estudiantes.component.ts
+++ src/app/estudiantes/estudiantes.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -9,18 +8,40 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class EstudiantesComponent {
-  estudiante = {
-    nombre: '',
-    apellido: '',
-    email: '',
-    telefono: ''
-  };
+  estudiantes = [
+    { nombre: 'Juan', apellido: 'Pérez', email: 'juan.perez@hotmail.com', telefono: '0982737190' },
+    { nombre: 'María', apellido: 'García', email: 'maria.garcia@gmail.com', telefono: '0999238183' },
+    { nombre: 'Pedro', apellido: 'Rodríguez', email: 'pedro.rodriguez@hotmail.com', telefono: '0984124289' }
+  ];
 
-  constructor(private http: HttpClient) {}
+  estudianteSeleccionado: any = {};
+  modoEdicion: boolean = false;
 
-  onSubmit() {
-    this.http.post('/api/students', this.estudiante).subscribe(() => {
-      alert('Registro exitoso!');
-    });
+  agregarEstudiante(): void {
+    this.estudianteSeleccionado = {};
+    this.modoEdicion = false;
+  }
+
+  editarEstudiante(estudiante: any): void {
+    this.estudianteSeleccionado = { ...estudiante };
+    this.modoEdicion = true;
+  }
+
+  guardarEstudiante(): void {
+    if (this.modoEdicion) {
+      const index = this.estudiantes.findIndex(e => e.nombre === this.estudianteSeleccionado.nombre);
+      this.estudiantes[index] = { ...this.estudianteSeleccionado };
+    } else {
+      const id = this.estudiantes.length + 1;
+      this.estudiantes.push({ id, ...this.estudianteSeleccionado });
+    }
+
+    this.estudianteSeleccionado = {};
+    this.modoEdicion = false;
+  }
+
+  eliminarEstudiante(estudiante: any): void {
+    const index = this.estudiantes.findIndex(e => e.nombre === estudiante.id);
+    this.estudiantes.splice(index, 1);
   }
 }
