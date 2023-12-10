6914459c8375cd9e300c3f17d273703079d6c6a3
Christopher <christopher.a31@hotmail.com>
2023-12-09 22:34:44 -0500

    Actualizacion componente estudiantes

--- src/app/estudiantes/estudiantes.component.ts
+++ src/app/estudiantes/estudiantes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EstudianteService } from '../servicios/estudiante.service';
 
 
 @Component({
@@ -8,40 +9,41 @@ import { Component } from '@angular/core';
 })
 
 export class EstudiantesComponent {
-  estudiantes = [
-    { nombre: 'Juan', apellido: 'Pérez', email: 'juan.perez@hotmail.com', telefono: '0982737190' },
-    { nombre: 'María', apellido: 'García', email: 'maria.garcia@gmail.com', telefono: '0999238183' },
-    { nombre: 'Pedro', apellido: 'Rodríguez', email: 'pedro.rodriguez@hotmail.com', telefono: '0984124289' }
-  ];
 
-  estudianteSeleccionado: any = {};
-  modoEdicion: boolean = false;
+  constructor(private servicio: EstudianteService){}
 
-  agregarEstudiante(): void {
-    this.estudianteSeleccionado = {};
-    this.modoEdicion = false;
-  }
+  dataEstudiante: any = {};
 
-  editarEstudiante(estudiante: any): void {
-    this.estudianteSeleccionado = { ...estudiante };
-    this.modoEdicion = true;
-  }
-
-  guardarEstudiante(): void {
-    if (this.modoEdicion) {
-      const index = this.estudiantes.findIndex(e => e.nombre === this.estudianteSeleccionado.nombre);
-      this.estudiantes[index] = { ...this.estudianteSeleccionado };
-    } else {
-      const id = this.estudiantes.length + 1;
-      this.estudiantes.push({ id, ...this.estudianteSeleccionado });
+  //MÉTODO POST
+  guardarEstudiante(id: string, nombre:string, apellido:string, email:string, telefono:string){
+    const ide:number=parseInt(id)
+    const temp ={
+      "id":ide,
+      "nombre": nombre,
+      "apellido": apellido,
+      "email": email,
+      "telefono": telefono
+    }
+    this.servicio.postEstudiantes(temp).subscribe();
     }
 
-    this.estudianteSeleccionado = {};
-    this.modoEdicion = false;
+//MÉTODO PUT
+  actualizarEstudiante(id: string, nombre:string, apellido:string, email:string, telefono:string){
+    const ide:number= parseInt(id)
+    const temp={
+      "id":ide,
+      "nombre": nombre,
+      "apellido": apellido,
+      "email": email,
+      "telefono": telefono
+    }
+    this.servicio.putEstudiantes(temp, ide).subscribe();
   }
 
-  eliminarEstudiante(estudiante: any): void {
-    const index = this.estudiantes.findIndex(e => e.nombre === estudiante.id);
-    this.estudiantes.splice(index, 1);
+  //MÉTODO DELETE
+  eliminarEstudiante(id:string){
+    const ide: number= parseInt(id)
+    this.servicio.deleteEstudiantes(ide).subscribe();
+
   }
 }
