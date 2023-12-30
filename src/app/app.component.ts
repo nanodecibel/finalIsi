372724bf6ed7b9e3a85a65eb203a0da3bc2c2617
Christopher <christopher.a31@hotmail.com>
2023-12-30 12:35:14 -0500

    Creacion login y prueba OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'finalIsi';
+  isLoggedIn: boolean = false;
+  username: string = 'Carlos';
+  password: string = '1234';
+
+  login() {
+    
+    if (this.username !== '' && this.password !== '') {
+      this.isLoggedIn = true;
+      console.log("Ingreso exitoso")
+    }
+    else{
+      console.log("Error")
+    }
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.username = '';
+    this.password = '';
+  }
 }
